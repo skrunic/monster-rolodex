6965fa1a7e0b7d7508ff738e1aa414ed92d0d770
Siniša <sinisa.krunic@triple-innovations.com>
2020-07-30 00:26:48 +0200

    Updates

---
+++ precache-manifest.edb25b3f6f0ecc84d943017634f22787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fc630fba954b2a74d81195d8ce9e3a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4c489ff89f1333f78ffe",
+    "url": "/monster-rolodex/static/css/main.97712deb.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c489ff89f1333f78ffe",
+    "url": "/monster-rolodex/static/js/main.f75d54cb.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
